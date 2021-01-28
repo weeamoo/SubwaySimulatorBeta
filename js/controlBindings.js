409c5a17685960454472ff6981db8c0b562a1596
Weeamoo <github@surfinontheinter.net>
2021-01-28 10:58:43 -0600

    Update controlBindings.js

--- js/controlBindings.js
+++ js/controlBindings.js
@@ -1,3 +1,4 @@
+
 //creates empty array to hold description of each keycode
 var keyCodeLookup = [];
 
@@ -12,6 +13,31 @@ keyCodeLookup[49] = "6";
 keyCodeLookup[49] = "7";
 keyCodeLookup[49] = "8";
 keyCodeLookup[49] = "9";
+keyCodeLookup[69] = "A";
+keyCodeLookup[66] = "B";
+keyCodeLookup[67] = "C";
+keyCodeLookup[68] = "D";
+keyCodeLookup[69] = "E";
+keyCodeLookup[70] = "F";
+keyCodeLookup[71] = "G";
+keyCodeLookup[72] = "H";
+keyCodeLookup[73] = "I";
+keyCodeLookup[74] = "J";
+keyCodeLookup[75] = "K";
+keyCodeLookup[76] = "L";
+keyCodeLookup[77] = "M";
+keyCodeLookup[78] = "N";
+keyCodeLookup[79] = "O";
+keyCodeLookup[80] = "P";
 keyCodeLookup[81] = "Q";
+keyCodeLookup[82] = "R";
+keyCodeLookup[83] = "S";
+keyCodeLookup[84] = "T";
+keyCodeLookup[85] = "U";
+keyCodeLookup[86] = "V";
+keyCodeLookup[87] = "W";
+keyCodeLookup[88] = "X";
+keyCodeLookup[89] = "Y";
+keyCodeLookup[90] = "Z";
 keyCodeLookup[109] = "NUM -";
 keyCodeLookup[173] = "-";
