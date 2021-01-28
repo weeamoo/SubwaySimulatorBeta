8ff0a5bafc52c88cfad3eac79af2ca8544982f40
Weeamoo <github@surfinontheinter.net>
2021-01-28 10:43:48 -0600

    Update controlBindings.js

--- js/controlBindings.js
+++ js/controlBindings.js
@@ -1,1 +1,17 @@
+//creates empty array to hold description of each keycode
+var keyCodeLookup = [];
 
+//add known codes in numeric order plz
+keyCodeLookup[48] = "0";
+keyCodeLookup[49] = "1";
+keyCodeLookup[50] = "2";
+keyCodeLookup[51] = "3";
+keyCodeLookup[52] = "4";
+keyCodeLookup[53] = "5";
+keyCodeLookup[49] = "6";
+keyCodeLookup[49] = "7";
+keyCodeLookup[49] = "8";
+keyCodeLookup[49] = "9";
+keyCodeLookup[81] = "Q";
+keyCodeLookup[109] = "NUM -";
+keyCodeLookup[173] = "-";
