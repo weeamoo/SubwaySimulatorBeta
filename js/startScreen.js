460c2861981cf9edc2ed6147ed7d448527db6de3
Weeamoo <github@surfinontheinter.net>
2021-01-30 00:16:03 -0600

    Update startScreen.js

--- js/startScreen.js
+++ js/startScreen.js
@@ -1,7 +1,14 @@
 var onStartMenu = true;
 
 function startGame () {
+
+	//hide title screen
 	document.getElementById("titleScreen").classList.add("invis");
+
+	//starting level
+	loadLevel("debug/test1", 0, 0);
+
+	//slef
 	onStartMenu = false;
 	paused = false;
 }
