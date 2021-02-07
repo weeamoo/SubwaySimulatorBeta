0979eae34b52fb068a8d43515a3a6522f3f34bf2
Weeamoo <github@surfinontheinter.net>
2021-02-06 22:09:47 -0600

    new level loader ready for testing

--- js/levelChange.js
+++ js/levelChange.js
@@ -68,6 +68,44 @@ function loadLevel (level, x, y) {
 	calcBounds();
 }
 
+function loadLevelNew (level, x, y) {
+
+	//clearing stuff
+	player.xSpeed = 0;
+	player.ySpeed = 0;
+	player.x = x;
+	player.y = y;
+
+	//calls to reset all world divs
+	lvlReset();
+
+	//sets geo function
+	eval("getNearestRail = lvlData." + level + ".geo;");
+
+	//level size
+	eval("levelWidth = lvlData." + level + ".width;");
+	eval("levelHeight = lvlData." + level + ".height;");
+
+	//css sheet for background
+	eval("document.getElementById(\"lvlLoaderCSS\").href = lvlData." + level + ".css;");
+
+	//HTML data for backgrounds
+	eval("document.getElementById(\"worldBg1\").innerHTML = lvlData." + level + ".bg1;");
+	eval("document.getElementById(\"worldBg2\").innerHTML = lvlData." + level + ".bg2;");
+	eval("document.getElementById(\"worldBg3\").innerHTML = lvlData." + level + ".bg3;");
+	eval("document.getElementById(\"worldBg4\").innerHTML = lvlData." + level + ".bg4;");
+	eval("document.getElementById(\"worldBg5\").innerHTML = lvlData." + level + ".bg5;");
+
+	//HTML data for foregrounds
+	eval("document.getElementById(\"worldFg1\").innerHTML = lvlData." + level + ".fg1;");
+	eval("document.getElementById(\"worldFg2\").innerHTML = lvlData." + level + ".fg2;");
+	eval("document.getElementById(\"worldFg3\").innerHTML = lvlData." + level + ".fg3;");
+	eval("document.getElementById(\"worldFg4\").innerHTML = lvlData." + level + ".fg4;");
+	eval("document.getElementById(\"worldFg5\").innerHTML = lvlData." + level + ".fg5;");
+	eval("document.getElementById(\"worldSFg5\").innerHTML = lvlData." + level + ".sfg5;");
+
+}
+
 //clears all world divs
 function lvlReset () {
 	document.getElementById('worldBg1').innerHTML = "";
