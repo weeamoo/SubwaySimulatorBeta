417dd4414b826b6c5f6c00398be556f29ee04b7f
Weeamoo <github@surfinontheinter.net>
2021-03-11 11:48:58 -0600

    Update startScreen.js

--- js/startScreen.js
+++ js/startScreen.js
@@ -1,5 +1,7 @@
 var onStartMenu = true;
 
+setTimeout(idleBackground(), 60000)
+
 function startGame () {
 
 	//hide title screen
@@ -18,4 +20,13 @@ function startGame () {
 
 function addMikuToTitle () {
 	document.getElementById("titleScreen").style.backgroundImage = "url(\"https://surfinontheinter.net/img/myspace/mikuback.jpg\")";
+}
+
+function idleBackground () {
+	if (onStartMenu == false) {
+	    
+		loadLevel(lvlData.debug.test4, 0, 0);
+		player.xSpeed = 0.1;
+	    
+	}
 }
