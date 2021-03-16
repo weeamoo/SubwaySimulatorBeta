868a53cca45bd490a97bec497869a424a9c9f076
Weeamoo <github@surfinontheinter.net>
2021-03-15 23:24:23 -0500

    Update wanderCar.js

--- entityTemplate/debug/wanderCar/wanderCar.js
+++ entityTemplate/debug/wanderCar/wanderCar.js
@@ -8,7 +8,7 @@ entityTemplate.wanderCar = function (name, x, y) {
 	output.name = name;
 	//modifies
 	output.id = "entity_" + name;
-	output.ai = ai.controller;
+	output.ai = ai.wanderCar;
 	output.html = "<div id=\"" + output.id + "\" style=\"left: 0%; bottom: 29.1111%;\"><img id=\"playerCarBodyImg\" class=\"playerCarImg\" src=\"/img/player/car.png\"><img id=\"playerCarDoorImg\" class=\"playerCarImg\" src=\"/img/player/door.png\"></div>";
 
 	return output;
