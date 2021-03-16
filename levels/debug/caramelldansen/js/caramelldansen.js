235f4cc2b6f1aa8b706d53518996e2b0ebfdc497
Weeamoo <github@surfinontheinter.net>
2021-03-16 10:56:20 -0500

    Update caramelldansen.js

--- levels/debug/caramelldansen/js/caramelldansen.js
+++ levels/debug/caramelldansen/js/caramelldansen.js
@@ -4,7 +4,7 @@ lvlData.debug.caramelldansen = [];
 lvlData.debug.caramelldansen.width = 1600;
 lvlData.debug.caramelldansen.height = 900;
 lvlData.debug.caramelldansen.css = "/levels/debug/caramelldansen/css/caramelldansen.css";
-lvlData.debug.caramelldansen.geo = function (x, y, dir) {player.speedLimit = 5;return 0;};
+lvlData.debug.caramelldansen.geo = function (x, y, dir) {return 0;};
 lvlData.debug.caramelldansen.bg1 = "<iframe src=\"https://www.youtube-nocookie.com/embed/PDJLvF1dUek?autoplay=1&amp;controls=0&amp;loop=1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen=\"\" width=\"100%\" height=\"100%\" frameborder=\"0\"></iframe>";
 lvlData.debug.caramelldansen.bg2 = "";
 lvlData.debug.caramelldansen.bg3 = "";
