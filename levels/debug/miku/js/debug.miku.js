a3fbf533857dc08c6c195e79fef3cc4601067389
Weeamoo <github@surfinontheinter.net>
2021-06-08 08:12:04 -0500

    Update debug.miku.js

--- levels/debug/miku/js/debug.miku.js
+++ levels/debug/miku/js/debug.miku.js
@@ -2,6 +2,8 @@ lvlData.debug.miku.width = 48000;
 lvlData.debug.miku.height = 900;	
 lvlData.debug.miku.css = "/levels/debug/miku/css/miku.css";
 lvlData.debug.miku.geo = function (x, y, dir) {return 0;};
+lvlData.debug.miku.speedLimit = function (x, y) {return 5;};
+lvlData.debug.miku.objects = function (x, y, entity) {};
 lvlData.debug.miku.bg1 = "<table id=\"gridMikuLevel\" cellspacing=\"0\"><tbody><tr><td><img id=\"backMikuPic1\" class=\"backMikuPic\" src=\"/levels/debug/miku/img/miku1.png\"></td><td><img id=\"backMikuPic2\" class=\"backMikuPic\" src=\"/levels/debug/miku/img/miku2.jpg\"></td><td><img id=\"backMikuPic3\" class=\"backMikuPic\" src=\"/levels/debug/miku/img/miku3.jpg\"></td><td><img id=\"backMikuPic4\" class=\"backMikuPic\" src=\"/levels/debug/miku/img/miku4.jpg\"></td><td><img id=\"backMikuPic5\" class=\"backMikuPic\" src=\"/levels/debug/miku/img/miku5.jpg\"></td></tr></tbody></table><iframe src=\"https://www.youtube-nocookie.com/embed/NocXEwsJGOQ?autoplay=1&amp;controls=0&amp;loop=1\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen=\"\" width=\"0px\" height=\"0px\" frameborder=\"0\"></iframe>";
 lvlData.debug.miku.bg2 = "";
 lvlData.debug.miku.bg3 = "";
